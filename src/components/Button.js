df7fd114048081e7c1c0b981d7db398bc76201f4
techieton-i <odejobiakin@gmail.com>
2022-03-09 18:34:08 +0100

    Navbar

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Button.scss';
+
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn-wide'];
+
+const COLOR = ['primary', 'blue', 'red', 'green'];
+
+export const Button = ({
+    children, type, onClick, buttonStyle, buttonSize, buttonColor
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonStyle : SIZES[0];
+
+    const checkButtonColor = COLOR.includes(buttonColor) ? buttonStyle : COLOR[0];
+
+    return (
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
+    );
+};
